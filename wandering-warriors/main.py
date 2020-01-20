a74b685d752e088b0e99481259853de054ca2351
DanielBeer <danielbeer1376@gmail.com>
2020-01-20 13:06:25 -0500

    WIP abacus widget design

--- wandering-warriors/main.py
+++ wandering-warriors/main.py
@@ -1,4 +1,5 @@
 from kivy.app import App
+from kivy.graphics import Color, Rectangle
 from kivy.lang import Builder
 from kivy.uix.screenmanager import ScreenManager, Screen
 from kivy.uix.widget import Widget
@@ -17,7 +18,123 @@ class TopMenu(Widget):
 
 
 class Abacus(Widget):
-    pass
+    BAR_W = 10
+    BORDER_W = 16
+    DIVIDER_OFFSET = 0.6
+    BEAD_SPACING = 8
+
+    def __init__(self, **kwargs):
+        super(Abacus, self).__init__(**kwargs)
+
+        self.n_bars = 10
+        self.n_top_beads = 1
+        self.n_bottom_beads = 4
+
+        print(self.pos)
+
+        with self.canvas:
+            Color(1, 1, 1, 1)
+
+            self.border = []
+
+            for i in range(4):
+                self.border.append(Rectangle(source='assets/graphics/wood.png'))
+
+            Color(1, 1, 1, 0.2)
+
+            self.border_highlight = [Rectangle(), Rectangle()]
+
+            Color(1, 1, 1, 1)
+
+            self.divider = Rectangle(source='assets/graphics/wood.png')
+
+            Color(1, 1, 1, 0.2)
+
+            self.border_highlight.append(Rectangle())
+
+            self.bar_rects = []
+            self.top_beads = []
+            self.bottom_beads = []
+
+            Color(0.8, 0.8, 0.8, 1)
+
+            for i in range(self.n_bars):
+                self.bar_rects.append([Rectangle(), Rectangle()])
+
+                top_beads = []
+
+                for j in range(self.n_top_beads):
+                    top_beads.append(Rectangle(source='assets/graphics/bead.png'))
+
+                self.top_beads.append(top_beads)
+
+                bottom_beads = []
+
+                for j in range(self.n_bottom_beads):
+                    bottom_beads.append(Rectangle(source='assets/graphics/bead.png'))
+
+                self.bottom_beads.append(bottom_beads)
+
+        self.bind(pos=self.update, size=self.update)
+        self.update()
+
+    def update(self, *args):
+        w = max(self.height / 20, self.BORDER_W)
+
+        self.border[0].pos = (self.x, self.y + self.height - w)
+        self.border[0].size = (self.width, w)
+
+        self.border[1].pos = self.pos
+        self.border[1].size = (self.width, w)
+
+        self.border[2].pos = self.pos
+        self.border[2].size = (w, self.height)
+
+        self.border[3].pos = (self.x + self.width - w, self.y)
+        self.border[3].size = (w, self.height)
+
+        inner_w = self.width - 2 * w
+
+        self.border_highlight[0].pos = (self.x, self.y + self.height - w / 5)
+        self.border_highlight[0].size = (self.width, w / 5)
+
+        self.border_highlight[1].pos = (self.x + w, self.y + 4 * w / 5)
+        self.border_highlight[1].size = (inner_w, w / 5)
+
+        self.divider.pos = (self.x + w, self.y + self.height * self.DIVIDER_OFFSET)
+        self.divider.size = (inner_w, w)
+
+        self.border_highlight[2].pos = (self.x + w, self.y + 4 * w / 5 + self.height * self.DIVIDER_OFFSET)
+        self.border_highlight[2].size = (inner_w, w / 5)
+
+        for i in range(self.n_bars):
+            bar = self.bar_rects[i]
+            spacing = inner_w / self.n_bars
+
+            x = self.x + w + spacing / 2 + spacing * i - self.BAR_W / 2
+
+            bar[0].pos = (x, self.y + w - w / 10)
+            bar[0].size = (self.BAR_W, self.height - w + w / 10 - self.height * (1 - self.DIVIDER_OFFSET))
+            
+            bar[1].pos = (x, self.y + self.height * self.DIVIDER_OFFSET + w - w / 10)
+            bar[1].size = (self.BAR_W, self.height * (1 - self.DIVIDER_OFFSET) - 2 * w + w / 10)
+
+            bead_w = spacing - self.BEAD_SPACING
+            bead_x = self.x + w + spacing * i + self.BEAD_SPACING / 2
+
+            top_beads = self.top_beads[i]
+
+            for j in range(self.n_top_beads):
+                top_beads[j].pos = (bead_x, self.y + self.height - self.BORDER_W - bead_w / 2 + j * bead_w / 2)
+                top_beads[j].size = (bead_w, bead_w / 2)
+
+            bottom_beads = self.bottom_beads[i]
+
+            for j in range(self.n_bottom_beads):
+                bottom_beads[j].pos = (bead_x, self.y + self.BORDER_W + j * bead_w / 2)
+                bottom_beads[j].size = (bead_w, bead_w / 2)
+
+
 
 
 class Ledger(Widget):
