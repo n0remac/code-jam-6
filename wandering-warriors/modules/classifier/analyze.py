01de9b3201060ecce1dba0212aee3975d8027d89
Ryan Gables <regables@gmail.com>
2020-01-23 20:17:38 -0800

    return numpy array

--- wandering-warriors/modules/classifier/analyze.py
+++ wandering-warriors/modules/classifier/analyze.py
@@ -12,18 +12,14 @@ def clean(data_in) -> np.array:
     x = (255 * (x - np.min(x)) / max_point).astype(int)
     y = (255 * (y - np.min(y)) / max_point).astype(int)
 
-    return x, y
+    # Visualize coordinates for development
+    plot(x, y)
 
-    # data_out = np.dstack((data_in[0::2], data_in[1::2]))[0]
-    # print(data_out.shape)
-    # print(data_out)
-    #
-    # return data_out
+    return np.dstack((x, y))
 
 
 def plot(x, y):
     c = cm.rainbow(np.linspace(0, 1, len(x)))
-
     plt.scatter(x=x, y=y, color=c)
 
     plt.axis('equal')
@@ -31,5 +27,5 @@ def plot(x, y):
 
 
 def analyze(raw_data):
-    x, y = clean(raw_data)
-    plot(x, y)
+    clean_data = clean(raw_data)
+    print(clean_data.shape)
